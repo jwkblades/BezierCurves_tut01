485a419debf80ef2b9a739a0203378a2d8d16121
James Blades <james.blades@colorado.edu>
2013-05-07 14:18:52 -0600

    Updated main.cpp to enable keyboard input

--- main.cpp
+++ main.cpp
@@ -2,13 +2,36 @@
 using namespace std;
 
 int main(void){
-	std::vector<Point> pts;
-	pts.push_back(Point(20, 20));
-	pts.push_back(Point(30, 0));
-	pts.push_back(Point(40, 40));
-	pts.push_back(Point(50, 20));
-	for(double t = 0; t <= 1; t += .01){
-		cout << bezier(pts, t) << endl;
+	std::vector<std::vector<Point> > pts;
+	char endPointCount = 0;
+	double inx;
+	double iny;
+	int ptCount = 0;
+	int endPoint;
+
+	cin >> ptCount;
+	for(int i = 0; i < ptCount; i++){
+		cin >> inx >> iny >> endPoint;
+		Point p(inx, iny);
+		if(endPointCount == 0 && endPoint == 1){
+			pts.push_back(std::vector<Point>());
+			pts[pts.size() - 1].push_back(p);
+			endPointCount++;
+			continue;
+		}
+		pts[pts.size() - 1].push_back(p);
+		if(endPointCount != 0 && endPoint == 1 && i != ptCount - 1){
+			pts.push_back(std::vector<Point>());
+			pts[pts.size() - 1].push_back(p);
+			endPointCount++;
+		}
+	}
+	
+
+	for(std::size_t i = 0; i < pts.size(); i++){
+		for(double t = 0; t <= 1; t += .1){
+			cout << bezier(pts[i], t) << endl;
+		}
 	}
 	return 0;
 }
@@ -17,7 +40,7 @@ Point bezier(std::vector<Point>& pts, double t){
 	Point p;
 	std::size_t len = pts.size();
 	for(std::size_t i = 0; i < len; i++){
-		p += pts[i] * (choose(len - 1, i) * pow(1 - t, len - 1 - i) * pow(t, i));
+		p += pts[i] * choose(len - 1, i) * pow(1 - t, len - 1 - i) * pow(t, i);
 	}
 	return p;
 }
